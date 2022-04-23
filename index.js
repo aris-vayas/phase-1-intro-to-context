13b8c7f9aa3fd3ef193b26b262bb5bb9157a557d
Aris Vayas <aris.vayas@gmail.com>
2022-04-23 10:23:54 -0400

    commit tree 2fe40e78ac13f1d06f1f20513afe43cce2063199

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+
+function createEmployeeRecord(arr){
+return {
+    firstName:arr[0],
+    familyName: arr[1],
+    title: arr[2],
+    payPerHour: arr[3],
+    timeInEvents:[],
+    timeOutEvents:[]
+}
+    
+}
+function createEmployeeRecords(){
+
+
+
+
+
+}
+function createEmployeeRecords(array){
+   let newArr= array.map(createEmployeeRecord())
+
+
+}
+createEmployeeRecords()
+createEmployeeRecord()
